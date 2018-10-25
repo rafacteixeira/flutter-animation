44feca694e3a5811a3c629fc9d8fef0a41e75be5
Rafael Cruz <rafacteixeira@gmail.com>
2018-10-24 21:27:56 -0300

    starting animation

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+        duration: Duration(seconds: 2),
+        vsync: this,
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation"),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+
+  Widget buildAnimation() {
+    return Cat();
+  }
+}
